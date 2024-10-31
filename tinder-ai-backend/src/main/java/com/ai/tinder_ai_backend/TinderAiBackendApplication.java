9641ec47df87915a730324e94d464b383c35161b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-30 22:34:27 -0500

    OpenAI secret key configured into zshrc file and docker is up and running with mongoDB

---
+++ tinder-ai-backend/src/main/java/com/ai/tinder_ai_backend/TinderAiBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ai.tinder_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiBackendApplication.class, args);
+		System.err.println("Tinder App is Up and Running");
+	}
+
+}
