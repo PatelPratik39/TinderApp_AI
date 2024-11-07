c96a7d208f9c568e1d576ba62dfc3af71dbf5cd1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-11-07 00:23:39 -0600

    Matches render on Match Component

--- tinder-ai-frontend/src/App.jsx
+++ tinder-ai-frontend/src/App.jsx
@@ -24,6 +24,14 @@ const saveSwipe = async (profileId) => {
   }
 };
 
+const fetchMatches = async (profileId) => {
+  const response = await fetch("http://localhost:8080/matches");
+  if (!response.ok) {
+    throw new Error("Failed to save Swipe");
+  }
+  return response.json();
+};
+
 const ProfileSelector = ({ profile, onSwipe }) =>
   profile ? (
     <div className="rounded-lg overflow-hidden bg-white shadow-lg">
@@ -57,38 +65,23 @@ const ProfileSelector = ({ profile, onSwipe }) =>
     <div>Loading...</div>
   );
 
-const MatchesList = ({ onSelectMatch }) => (
+const MatchesList = ({ matches, onSelectMatch }) => (
   <div className="rounded-lg shadow-lg p-4">
     <h2 className="text-2xl font-bold mb-4 text-left"> Matches</h2>
     <ul>
-      {[
-        {
-          id: 1,
-          firstName: "Mangoo",
-          lastName: "Moti",
-          imageUrl:
-            "http://127.0.0.1:8081/018aafd0-6a0d-4f38-b2ca-45c0c9b13fa2.jpg"
-        },
-        {
-          id: 2,
-          firstName: "Chunni",
-          lastName: "Bai",
-          imageUrl:
-            "http://127.0.0.1:8081/017e4530-49b4-4937-8adf-985a82595d53.jpg"
-        }
-      ].map((match) => (
-        <li key={match.id} className="mb-2">
+      {matches.map((match) => (
+        <li key={match.profile.id || match.id} className="mb-2">
           <button
             className="w-full rounded flex item-center hover:bg-gray-100"
             onClick={onSelectMatch}
           >
             <img
-              src={match.imageUrl}
+              src={"http://127.0.0.1:8081/" + match.profile.imageUrl}
               className="w-16 h-16 rounded-full mr-3 object-cover"
             />
             <span>
               <h3 className="font-bold">
-                {match.firstName} {match.lastName}
+                {match.profile.firstName} {match.profile.lastName}
               </h3>
             </span>
           </button>
@@ -145,17 +138,28 @@ function App() {
       console.error(error);
     }
   };
+  const loadMatches = async () => {
+    try {
+      const matches = await fetchMatches();
+      setMatches(matches);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     loadRandomProfile();
+    loadMatches();
   }, []);
 
   const [currentScreen, setCurrentScreen] = useState("profile");
   const [currentProfile, setCurrentProfile] = useState(null);
+  const [matches, setMatches] = useState([]);
 
-  const onSwipe = (profileId, direction) => {
+  const onSwipe = async(profileId, direction) => {
     if (direction === "right") {
-      saveSwipe(profileId);
+      await saveSwipe(profileId);
+      await loadMatches();
     }
     loadRandomProfile();
   };
@@ -165,7 +169,12 @@ function App() {
       case "profile":
         return <ProfileSelector profile={currentProfile} onSwipe={onSwipe} />;
       case "matches":
-        return <MatchesList onSelectMatch={() => setCurrentScreen("chat")} />;
+        return (
+          <MatchesList
+            matches={matches}
+            onSelectMatch={() => setCurrentScreen("chat")}
+          />
+        );
       case "chat":
         return <ChatScreen />;
       default:
@@ -181,8 +190,6 @@ function App() {
           <MessageCircle onClick={() => setCurrentScreen("matches")} />
         </nav>
         {renderScreen()}
-        {/* <ProfileSelector /> */}
-        {/* <MatchesList /> */}
       </div>
     </>
   );
